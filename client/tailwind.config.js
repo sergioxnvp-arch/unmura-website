5bceef5bf7500ed344655d4cbcdab86624326eb6
sergioxnvp-arch <sergioxnvp@gmail.com>
2026-06-23 03:07:02 +0700

    Create tailwind.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        unmura: { blue: '#1E3A8A', light: '#F3F4F6' }
+      }
+    }
+  },
+  plugins: []
+};
