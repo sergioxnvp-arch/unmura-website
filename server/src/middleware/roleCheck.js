fccc11086e268b48b8845ec8e0f28a92002d6589
sergioxnvp-arch <sergioxnvp@gmail.com>
2026-06-23 02:42:20 +0700

    Create roleCheck.js

---
+++ server/src/middleware/roleCheck.js
@@ -0,0 +1,10 @@
+const isRole = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Akses terbatas' });
+    }
+    next();
+  };
+};
+
+module.exports = isRole;
