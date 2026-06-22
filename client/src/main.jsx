55fd0fce54700ebcb1122683e6513b1d7b39d7ce
sergioxnvp-arch <sergioxnvp@gmail.com>
2026-06-23 04:23:40 +0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import App from './App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <h1 style={{ textAlign: 'center', marginTop: '50px' }}>UNMURA Test - React Berfungsi!</h1>
 );
