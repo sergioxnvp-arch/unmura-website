6c48b48bb7fabaa6b2375d7c9bf89568343b79af
sergioxnvp-arch <sergioxnvp@gmail.com>
2026-06-23 04:42:52 +0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,16 @@
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 function App() {
-  return <h2>App Loaded</h2>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <h1 className="text-center py-20 text-2xl">Selamat datang di UNMURA</h1>
+      </main>
+      <Footer />
+    </div>
+  );
 }
+
 export default App;
