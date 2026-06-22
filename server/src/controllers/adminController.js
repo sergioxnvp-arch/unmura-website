f7dd1fdc485afa1c948231bfb6909bc3181398c1
sergioxnvp-arch <sergioxnvp@gmail.com>
2026-06-23 02:47:04 +0700

    Create adminController.js

---
+++ server/src/controllers/adminController.js
@@ -0,0 +1,23 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany({
+      select: { id: true, email: true, role: true, nama: true, prodi: true },
+    });
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await prisma.user.delete({ where: { id } });
+    res.json({ message: 'User deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
